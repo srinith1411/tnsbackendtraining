09c37685f9e6a6dfdcde5c05efe98ef8a1ade71f
srinith1411 <srinith014@gmail.com>
2025-08-05 22:33:34 +0530

    TYPES OF VARIABLES DEMONSTRATION

--- src/srinith/variablesDemo.java
+++ src/srinith/variablesDemo.java
@@ -1,8 +1,21 @@
 package srinith;
 
 public class variablesDemo {
+    String name; // INSTANCE VARIABLE
+    static String collegeName = "KPRIT"; // STATIC VARIABLE
+
+    void display() {
+        name = "srinith";  // or this.name = "srinith";
+        System.out.println("DATA IN INSTANCE VARIABLE: " + name);
+        System.out.println("DATA IN STATIC VARIABLE: " + variablesDemo.collegeName);
+    }
+}
+
+class variablesDemoMain {
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
+        variablesDemo ob = new variablesDemo();
+        int a = 10;  // LOCAL VARIABLE
+        System.out.println("DATA IN LOCAL VARIABLE: " + a);
+        ob.display();
     }
 }
