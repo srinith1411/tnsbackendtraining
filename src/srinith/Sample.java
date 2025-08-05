359457b335d8c0dfb22edeaf3cc83492efa201b0
aillasaiparth <aillasaiparth@gmail.com>
2025-08-05 22:01:46 +0530

    sample program

---
+++ src/srinith/Sample.java
@@ -0,0 +1,11 @@
+package srinith;
+
+public class Sample {
+	static  int add(int a,int b) {
+			return a+b;
+		}
+public static void main(String args[]) {
+	System.out.print("HELLO,WELCOME TO TNSIF");
+	System.out.println(add(2,3));
+}
+}
