871ef28a76b32f7154160067ba6f9c6143c6cc32
srinith1411 <srinith014@gmail.com>
2025-08-07 17:23:06 +0530

    DEMONSTRATION OF LOOPS USING A BASIC PROGRAM

---
+++ src/srinith/loopsDemo.java
@@ -0,0 +1,30 @@
+package srinith;
+
+public class  loopsDemo{
+}
+ class  loopsDemoMain{
+    public static void main(String[] args) {
+
+        // For Loop
+        System.out.println("Using for loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+
+        // While Loop
+        System.out.println("\nUsing while loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("Number: " + j);
+            j++;
+        }
+
+        // Do-While Loop
+        System.out.println("\nUsing do-while loop:");
+        int k = 1;
+        do {
+            System.out.println("Number: " + k);
+            k++;
+        } while (k <= 5);
+}
+}
