0d94fc23680509dc26ac35657f3c452bfcde21e6
srinith1411 <srinith014@gmail.com>
2025-08-24 17:15:24 +0530

    Added new tnsjavaoops package with OOPs programs

---
+++ src/tnsjavaoops/InheritanceDemo.java
@@ -0,0 +1,21 @@
+package tnsjavaoops;
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog acquires animal class properties.");
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Animal obj = new Dog();
+        obj.sound();
+    }
+}
