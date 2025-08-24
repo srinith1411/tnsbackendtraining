951c277ba4063a815ef4a8a834b093891de24124
srinith1411 <srinith014@gmail.com>
2025-08-24 17:39:07 +0530

    oops concept

---
+++ src/tnsjavaoops/Ticket.java
@@ -0,0 +1,46 @@
+
+package tnsjavaoops;
+
+class Ticket {
+    // Private fields
+    private String passengerName;
+    private int seatNumber;
+    private boolean booked;
+
+    // Constructor
+    public Ticket(String passengerName, int seatNumber) {
+        this.passengerName = passengerName;
+        this.seatNumber = seatNumber;
+        this.booked = false; // initially not booked
+    }
+
+    // Getter and Setter for passengerName
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    // Getter for seatNumber
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    // Method to book the ticket
+    public void bookTicket() {
+        if (!booked) {
+            booked = true;
+            System.out.println("Ticket booked successfully for " + passengerName +
+                               ", Seat No: " + seatNumber);
+        } else {
+            System.out.println("Sorry, Seat No: " + seatNumber + " is already booked.");
+        }
+    }
+
+    // Method to check if ticket is booked
+    public boolean isBooked() {
+        return booked;
+    }
+}
---
+++ src/tnsjavaoops/TicketBooking.java
@@ -0,0 +1,20 @@
+package tnsjavaoops;
+public class TicketBooking {
+    public static void main(String[] args) {
+        // Create ticket objects
+        Ticket ticket1 = new Ticket("Srinith", 101);
+        Ticket ticket2 = new Ticket("Rahul", 102);
+
+        // Book tickets
+        ticket1.bookTicket();
+        ticket2.bookTicket();
+
+        // Try booking same seat again
+        ticket1.bookTicket();
+
+        // Access data via getters
+        System.out.println("Passenger Name: " + ticket2.getPassengerName() +
+                           ", Seat No: " + ticket2.getSeatNumber() +
+                           ", Booked: " + ticket2.isBooked());
+    }
+}
