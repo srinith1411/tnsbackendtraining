961fc7e8e95ade4280395b15ead5db76a6d5214e
srinith1411 <srinith014@gmail.com>
2025-09-02 22:07:38 +0530

    STACKS DEMO PROGRAM

---
+++ src/tnsjavacollectionsframework/StackDemo.java
@@ -0,0 +1,34 @@
+package tnsjavacollectionsframework;
+
+import java.util.*;
+public class StackDemo {
+    public static void main(String[] args) {
+        // Create a stack
+        Stack<Integer> stack = new Stack<>();
+
+        // Push elements
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Initial Stack: " + stack);
+
+        // Peek top
+        System.out.println("Peek top: " + stack.peek());
+
+        // Pop top
+        System.out.println("Popped: " + stack.pop());
+        System.out.println("After pop: " + stack);
+
+        // Search for element
+        System.out.println("Position of 10: " + stack.search(10));
+
+        // Traversal using Iterator
+        System.out.print("Iterator traversal: ");
+        Iterator<Integer> itr = stack.iterator();
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+    }
+}
+
---
+++ src/tnsjavacollectionsframework/VectorDemo.java
@@ -0,0 +1,31 @@
+package tnsjavacollectionsframework;
+import java.util.*;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        // Create a vector
+        Vector<String> vector = new Vector<>();
+
+        // Add elements
+        vector.add("Apple");
+        vector.add("Banana");
+        vector.add("Cherry");
+
+        System.out.println("Vector: " + vector);
+
+        // Access elements
+        System.out.println("First element: " + vector.firstElement());
+        System.out.println("Last element: " + vector.lastElement());
+
+        // Remove an element
+        vector.remove("Banana");
+        System.out.println("After removing Banana: " + vector);
+
+        // Traversal using Iterator
+        System.out.print("Iterator traversal: ");
+        Iterator<String> itr = vector.iterator();
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+    }
+}
